b0b041a6efdb85d8b4349f14d4d8e5810769ed3a
KiraQueensThirdBomb <pluta6786@gmail.com>
2025-11-24 00:04:57 +0100

    Modify application lists in config.sh
    Updated the lists of applications to install from Pacman and AUR.

--- config.sh
+++ config.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-apps_pacman=("hyprland" "")
-apps_aur=("matugen-bin")
+apps_pacman=("hyprland" "waybar" "kitty" "dolphin" "wofi" "rofi")
+apps_aur=("matugen-bin" "nerd-fonts")
 echo start installing aur
 sudo pacman -S --needed base-devel git
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 echo aur setup finished
+cd ~
+echo installing reqired stuff
+sudo pacman -Syu
+sudo pacman -S "${apps_pacman[@]}"
+yay -S "${apps_aur}"
+git clone https://github.com/LGFae/swww.git
+cargo build --release
+echo reqirement instalantion should be finished
+sudo cp ~/dotshome
