85255802f7faf5ce9a4b1cc9ed212c4ac3db4654
KiraQueensThirdBomb <pluta6786@gmail.com>
2025-11-24 00:18:06 +0100

    Update config.sh for file configuration and cleanup
    Added commands for file configuration and cleanup.

--- config.sh
+++ config.sh
@@ -15,4 +15,13 @@ yay -S "${apps_aur}"
 git clone https://github.com/LGFae/swww.git
 cargo build --release
 echo reqirement instalantion should be finished
-sudo cp ~/dotshome
+echo starting file configuration
+sudo rm -r /.config/hypr/
+sudo cp -r ~/dotshome/hypr ~/.config/
+sudo rm -r /.config/waybar/
+sudo cp -r ~/dotshome/waybar ~/.config/
+sudo rm -r /.config/matugen/
+sudo cp -r ~/dotshome/matugen ~/.config/
+sudo cp -r ~/dotshome/Wallpapers ~/Pictures/
+matugen image Pictures/Wallpapers/wall1.jpg
+echo file configuration should be finished in case of something not working fix it yourself
