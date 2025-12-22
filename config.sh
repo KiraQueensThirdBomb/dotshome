d98eade4b16250bb03717936792b4c1637550ff2
ricykage <pluta6786@gmail.com>
2025-12-22 21:37:01 +0100

    Modify font installation to use local fonts
    Updated font installation process to use local fonts from dotshome instead of downloading from GitHub.

--- config.sh
+++ config.sh
@@ -14,12 +14,7 @@ yay -S "${apps_aur[@]}"
 sudo pacman -S ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick #yazi dependencies
 git clone https://github.com/LazyVim/starter ~/.config/nvim #lazyvim install
 rm -rf ~/.config/nvim/.git
-git clone https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/DepartureMono.zip #fonts install
-git clone https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/ProFont.zip
-unzip ~/Downloads/DepartureMono.zip
-unzip ~/Downloads/ProFont.zip
-sudo cp -r ~/Downloads/DepartureMono/ /usr/share/fonts/
-sudo cp -r ~/Downloads/ProFont/ /usr/share/fonts/
+sudo cp -r ~/dotshome/DepartureMono/ /usr/share/fonts/ # copy departuremono 3.4 to global fonts directory
 echo reqirement instalantion should be finished
 echo starting file configuration #cp files to /.config
 rm -rf ~/.config/hypr/
@@ -35,7 +30,7 @@ cp -r ~/dotshome/rofi ~/.config/
 rm -rf ~/.config/yazi/
 cp -r ~/dotshome/yazi/ ~/.config/
 sudo cp -r ~/dotshome/darkblud/ /usr/share/themes/ #cp the gtk theme to the themes directory
-gsettings set org.gnome.desktop.interface gtk-theme "darkblud" #apply the "darkblud gtk theme"
+gsettings set org.gnome.desktop.interface gtk-theme "darkblud" #apply the "darkblud" gtk theme
 cp -r ~/dotshome/Wallpapers ~/Wallpapers/
 matugen image Pictures/Wallpapers/wall1.jpg #set wallpaper and generate colors
 echo file configuration should be finished in case of something not working fix it yourself
